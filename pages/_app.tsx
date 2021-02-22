64055cbb77acf395f970bdac7efec66436b98860
Anh Do <dinhanhdo95@gmail.com>
2021-02-22 22:57:35 +0700

    Setup styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
 import store from '@store';
+import '@styles/index.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
